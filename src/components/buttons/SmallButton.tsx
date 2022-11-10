7c148501afa178c60107a6ea9447efd99084280c
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-11 00:05:21 +0200

    added: props and its types

--- src/components/buttons/SmallButton.tsx
+++ src/components/buttons/SmallButton.tsx
@@ -2,12 +2,23 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 
-const SmallButton: React.FC = () => {
+interface ButtonProps {
+  text: string
+  bgColor: 'red' | 'blue'
+}
+
+/**
+ * @description display button
+ * @param text button's name
+ * @param bgColor background color of button
+ * @returns JSX Element
+ */
+const SmallButton = ({ text, bgColor }: ButtonProps) => {
   return (
     <>
       <Box sx={{ padding: '5px' }}>
-        <Button size='large' variant='contained'>
-          ADD
+        <Button size='large' variant='contained' sx={{ bgcolor: `${bgColor}` }}>
+          {text}
         </Button>
       </Box>
     </>

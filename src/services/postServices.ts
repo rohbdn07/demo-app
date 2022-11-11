6bcfb42f5cc151b5bce622725cb872a586bd84b5
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-11 22:31:18 +0200

    removed unused code, use of tryCatch

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -1,14 +1,6 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 import { PostModel } from '../models/redux-models'
 
-export interface AxiosResponse<T = never> {
-  data: T
-  status: number
-  statusText: string
-  headers: Record<string, string>
-  config: AxiosRequestConfig<T>
-  request?: any
-}
 /**
  * @axiosFetchAuthAPI baseurl to connect frontend to backend.
  * @baseurl "https://jsonplaceholder.typicode.com"
@@ -22,12 +14,6 @@ const axiosFetchAPI: AxiosInstance & AxiosRequestConfig = axios.create({
   },
 })
 
-// const responseBody = (response: AxiosResponse) => response.data
-
-// const postRequests = {
-//   get: async (url: string) => await axiosFetchAPI.get(url),
-// }
-
 /** fetch all posts from baseUrl */
 const getAllPost = async (): Promise<PostModel[]> => {
   const response = await axiosFetchAPI.get<PostModel[]>('posts')
@@ -36,13 +22,9 @@ const getAllPost = async (): Promise<PostModel[]> => {
 }
 
 /** fetch just a particular post by its ID from baseUrl */
-const getPostById = async (id: number): Promise<PostModel | undefined> => {
-  try {
-    const response = await axiosFetchAPI.get<PostModel>(`post/${id}`)
-    return response?.data
-  } catch (error) {
-    console.log('the error', error)
-  }
+const getPostById = async (id: number): Promise<PostModel> => {
+  const response = await axiosFetchAPI.get<PostModel>(`post/${id}`)
+  return response?.data
 }
 
 export default {

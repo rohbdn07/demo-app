e5a7c3ffe3ff5e359373eb9c0d2bcc9939e893b8
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-11 15:00:15 +0200

    use of react-redux for state management

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
-const container = document.getElementById('root') as HTMLElement
-const root = createRoot(container)
+const container = document.getElementById('root')
+const root = createRoot(container!)
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
 

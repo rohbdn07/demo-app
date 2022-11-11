ade229bb9b5927c0bf218dee44bd45eb79a8a3d3
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-12 01:37:14 +0200

    added: a func to do fetch action, dispatch action for manage error

--- src/redux/actions/posts/post-actions.ts
+++ src/redux/actions/posts/post-actions.ts
@@ -1,18 +1,32 @@
 import { PostModel } from '../../../models/redux-models'
 import postService from '../../../services/postServices'
-import { setAllPost } from '../../reducer-slices/post-slices'
+import { setAllPost, setErrorMessage, setParticularPost } from '../../reducer-slices/post-slices'
 import { AppThunk } from '../../store'
 
-const fetchPosts = (): AppThunk => {
+export const fetchPosts = (): AppThunk => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return async (dispatch, getState) => {
-    try {
-      const postResponse: PostModel[] = await postService.getAllPost()
-      dispatch(setAllPost(postResponse))
-    } catch (error) {
-      console.log('Error', error)
+    if (getState().post.allPosts.length === 0) {
+      try {
+        const postResponse: PostModel[] = await postService.getAllPost()
+        dispatch(setAllPost(postResponse))
+      } catch (error) {
+        console.log('Error', error)
+        dispatch(setErrorMessage('Opps! something went wrong!!'))
+      }
     }
   }
 }
 
-export default fetchPosts
+export const fetchParticularPostById = (postId: number): AppThunk => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  return async (dispatch, getState) => {
+    try {
+      const response: PostModel = await postService.getPostById(postId)
+      dispatch(setParticularPost(response))
+    } catch (error) {
+      console.log('ERROR while fetching post by postID', error)
+      dispatch(setErrorMessage('Opps! something went wrong!!'))
+    }
+  }
+}

bb5310223b3d183bde6f2da1a0d0acf6764046e0
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-12 11:22:41 +0200

    refactor code and updated, moved return list to another component

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,73 +1,39 @@
 import React from 'react'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemText from '@mui/material/ListItemText'
-import DeleteIcon from '@mui/icons-material/Delete'
-import IconButton from '@mui/material/IconButton'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import Avatar from '@mui/material/Avatar'
-import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
 
 import { useAppSelector } from '../../redux/hooks'
 import usePagination from '../../hooks/Pagination'
 import PaginationEl from '../pagination/PaginationEl'
+import Post from './Post'
 
 /**
  * @description it display the list of posts
- * @returns JSX Element of post lists
+ * @returns JSX Element of post list along with pagination component
  */
 const PostList: React.FC = () => {
-  const posts = useAppSelector((state) => state.post.allPosts)
+  const { allPosts, postById } = useAppSelector((state) => state.post)
   const [page, setPage] = React.useState(0)
 
-  const checkPosts = (): boolean => posts.length !== 0
+  const checkPosts = (): boolean => allPosts.length !== 0
+  const checkParticularPost = (): boolean => postById.id !== 0
 
   const itemsPerPage = 10
-  const { currentData, maxPage, moveToNextPage } = usePagination({ data: posts, itemsPerPage })
+  const { currentData, maxPage, moveToNextPage } = usePagination({ data: allPosts, itemsPerPage })
 
-  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    console.log(value)
+  const handlePageChange = (event: React.ChangeEvent<unknown>, value: number): void => {
     setPage(value)
     moveToNextPage(value)
   }
 
   return (
     <>
-      {checkPosts() &&
-        currentData()?.map((post) => (
-          <List key={post?.id} sx={{ width: '100%', bgcolor: 'background.paper' }}>
-            <ListItem alignItems='flex-start'>
-              <ListItemAvatar>
-                <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
-              </ListItemAvatar>
-              <ListItemText
-                primary={`${post.title}`}
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: 'inline' }}
-                      component='span'
-                      variant='body2'
-                      color='text.primary'
-                    >
-                      User id: {post?.userId}
-                    </Typography>
-                    {`- ${post?.body}`}
-
-                    <IconButton edge='end' aria-label='delete'>
-                      <DeleteIcon />
-                    </IconButton>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider variant='inset' component='li' />
-          </List>
-        ))}
-      {posts.length > 0 && (
+      {checkParticularPost() ? (
+        <Post data={postById} />
+      ) : (
+        checkPosts() && currentData()?.map((post) => <Post key={post.id} data={post} />)
+      )}
+      {allPosts.length > 0 && (
         <PaginationEl
-          data={posts}
+          data={allPosts}
           totalPage={maxPage}
           currentPage={page}
           defaultPage={1}

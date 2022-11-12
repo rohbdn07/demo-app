9fdd342f1968359e17b465365345d74efe1ebdc2
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2022-11-12 11:27:54 +0200

    added: a new post item component

---
+++ src/components/posts/Post.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import Divider from '@mui/material/Divider'
+import { Box } from '@mui/material'
+
+import { PostModel } from '../../models/redux-models'
+
+/** interfaces */
+interface PostProps {
+  data: PostModel
+}
+
+/** Post component accepts props and render Post according to given props */
+const Post = (props: PostProps) => {
+  return (
+    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <ListItem alignItems='flex-start'>
+        <ListItemAvatar>
+          <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' />
+        </ListItemAvatar>
+        <ListItemText
+          primary={`${props.data.title}`}
+          secondary={
+            <React.Fragment>
+              <Box sx={{ display: 'block' }} component='span'>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component='span'
+                  variant='body2'
+                  color='text.primary'
+                >
+                  UserID: {props.data.userId}, PostID: {props.data.id}
+                </Typography>
+                <Typography
+                  sx={{ display: 'block' }}
+                  component='span'
+                >{`- ${props.data.body}`}</Typography>
+              </Box>
+            </React.Fragment>
+          }
+        />
+        <IconButton edge='end' aria-label='delete'>
+          <DeleteIcon />
+        </IconButton>
+      </ListItem>
+      <Divider variant='inset' component='li' />
+    </List>
+  )
+}
+
+export default Post
